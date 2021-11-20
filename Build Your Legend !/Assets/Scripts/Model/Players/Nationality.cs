dbdbff8c5428ebad021a682f86254cedcec9a170
Karthiyian <enzo.le.fur@cegetel.net>
2021-11-20 23:40:46 +0100

    LEC BOT

--- Build Your Legend !/Assets/Scripts/Model/Players/Nationality.cs
+++ Build Your Legend !/Assets/Scripts/Model/Players/Nationality.cs
@@ -1,4 +1,4 @@
 ﻿public enum Nationality
 {
-    French,English,Korean,Chinese,Belgium,Italian,Spanish,Portugish,Swedish,Norvegian,Danish,Russian,Greek,American,Canadian,Turkish,German,Japanese,Swiss,Australian,Romanian,Polish,Bulgarian
+    French,English,Korean,Chinese,Belgium,Italian,Spanish,Portugish,Swedish,Norvegian,Danish,Russian,Greek,American,Canadian,Turkish,German,Japanese,Swiss,Australian,Romanian,Polish,Bulgarian,Czech
 }
