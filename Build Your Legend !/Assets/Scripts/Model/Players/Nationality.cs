dac25def48f31d1e9e1df3d831258ece727a3638
Karthiyian <enzo.le.fur@cegetel.net>
2021-11-23 23:22:34 +0100

    tweaks

--- Build Your Legend !/Assets/Scripts/Model/Players/Nationality.cs
+++ Build Your Legend !/Assets/Scripts/Model/Players/Nationality.cs
@@ -1,4 +1,4 @@
 ﻿public enum Nationality
 {
-    French,English,Korean,Chinese,Belgium,Italian,Spanish,Portugish,Swedish,Norvegian,Danish,Russian,Greek,American,Canadian,Turkish,German,Japanese,Swiss,Australian,Romanian,Polish,Bulgarian,Czech
+    French,English,Korean,Chinese,Belgium,Italian,Spanish,Portugish,Swedish,Norvegian,Danish,Russian,Greek,American,Canadian,Turkish,German,Japanese,Swiss,Australian,Romanian,Polish,Bulgarian,Czech,Slovak
 }
